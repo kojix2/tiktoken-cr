33c21ccb737a55e07712058ae4ff523ab682b7ad
kojix2 <2xijok@gmail.com>
2023-08-05 14:22:36 +0900

    Deleted core_bpe_wrapper.rs and simplified functionality in lib.rs

--- tiktoken_crystal/src/lib.rs
+++ tiktoken_crystal/src/lib.rs
@@ -1,52 +1,30 @@
-mod core_bpe_wrapper;
+use std::ffi::CStr;
+use std::os::raw::c_char;
 
-use core_bpe_wrapper::CoreBPEWrapper;
-use magnus::{define_module, function, prelude::*, Error, method, class, RModule, ExceptionClass};
-
-fn r50k_base() -> CoreBPEWrapper {
-    let core_bpe = tiktoken_rs::r50k_base().unwrap();
-    CoreBPEWrapper::new(core_bpe)
-}
-fn p50k_base() -> CoreBPEWrapper {
-    let core_bpe = tiktoken_rs::p50k_base().unwrap();
-    CoreBPEWrapper::new(core_bpe)
-}
-fn p50k_edit() -> CoreBPEWrapper {
-    let core_bpe = tiktoken_rs::p50k_edit().unwrap();
-    CoreBPEWrapper::new(core_bpe)
-}
-fn cl100k_base() -> CoreBPEWrapper {
-    let core_bpe = tiktoken_rs::cl100k_base().unwrap();
-    CoreBPEWrapper::new(core_bpe)
-}
-
-fn module() -> Result<RModule, magnus::Error> {
-    define_module("Tiktoken")
-}
-
-fn uncicode_error() -> Result<ExceptionClass, magnus::Error> {
-    module()?.define_error("UnicodeError", magnus::exception::standard_error())
-}
-
-#[magnus::init]
-fn init() -> Result<(), Error> {
-    let module = module()?;
-
-    let factory_module = module.define_module("BpeFactory")?;
-    factory_module.define_singleton_method("r50k_base", function!(r50k_base, 0))?;
-    factory_module.define_singleton_method("p50k_base", function!(p50k_base, 0))?;
-    factory_module.define_singleton_method("p50k_edit", function!(p50k_edit, 0))?;
-    factory_module.define_singleton_method("cl100k_base", function!(cl100k_base, 0))?;
-
-
-    let ext_module = module.define_module("Ext")?;
-    let bpe_class = ext_module.define_class("CoreBPE", class::object())?;
-
-    bpe_class.define_method("encode_ordinary", method!(CoreBPEWrapper::encode_ordinary, 1))?;
-    bpe_class.define_method("encode", method!(CoreBPEWrapper::encode, 2))?;
-    bpe_class.define_method("encode_with_special_tokens", method!(CoreBPEWrapper::encode_with_special_tokens, 1))?;
-
-
-    bpe_class.define_method("decode", method!(CoreBPEWrapper::decode, 1))?;
-    Ok(())
+#[no_mangle]
+pub extern "C" fn tanuki(s: *const c_char) -> u32 {
+    if s.is_null() {
+        eprintln!("Null pointer provided!");
+        return 0;
+    }
+    unsafe {
+        let raw = CStr::from_ptr(s);
+        match raw.to_str() {
+            Ok(valid_str) => {
+                let bpe = match tiktoken_rs::p50k_base() {
+                    Ok(bpe) => bpe,
+                    Err(e) => {
+                        eprintln!("Failed to create BPE: {:?}", e);
+                        return 0;
+                    }
+                };
+                let tokens = bpe.encode_with_special_tokens(valid_str);
+                tokens.len() as u32
+            },
+            Err(_) => {
+                eprintln!("Invalid UTF-8 sequence provided!");
+                0
+            }
+        }
+    }
 }
