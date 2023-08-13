0820bb9d802ff81067faa21d64eee9ee5f71a3bc
kojix2 <2xijok@gmail.com>
2023-08-13 00:14:11 +0000

    Added num_tokens_from_messages

--- spec/tiktoken_spec.cr
+++ spec/tiktoken_spec.cr
@@ -2,4 +2,22 @@ require "spec"
 require "../src/tiktoken.cr"
 
 describe Tiktoken do
+  describe "#num_tokens_from_messages" do
+    model = "gpt-4"
+    messages = [
+      {
+        "role"    => "system",
+        "content" => "You are a helpful assistant that only speaks French.",
+      },
+      {
+        "role"    => "user",
+        "content" => "Hello, how are you?",
+      },
+      {
+        "role"    => "assistant",
+        "content" => "Parlez-vous francais?",
+      },
+    ]
+    Tiktoken.num_tokens_from_messages(model, messages).should eq(36)
+  end
 end
--- src/tiktoken.cr
+++ src/tiktoken.cr
@@ -10,4 +10,37 @@ module Tiktoken
   def self.encoding_for_model(model_name)
     Encoding.for_model(model_name)
   end
+
+  def self.num_tokens_from_messages(model, messages)
+    num_messages = messages.size
+    messages_ptr = Pointer(LibTiktoken::ChatCompletionRequestMessage).malloc(num_messages)
+    messages.each_with_index do |message, i|
+      role = message["role"]
+      content = message.has_key?("content") ? message["content"].to_unsafe : Pointer(LibC::Char).null
+      name = message.has_key?("name") ? message["name"].to_unsafe : Pointer(LibC::Char).null
+      function_call =\
+        if message.has_key?("function_call")
+          Pointer(LibTiktoken::FunctionCall).malloc(1) do
+            LibTiktoken::FunctionCall.new(
+              name: message["function_call"]["name"],
+              arguments: message["function_call"]["arguments"]
+            )
+          end
+        else
+          Pointer(LibTiktoken::FunctionCall).null
+        end
+      messages_ptr[i] = LibTiktoken::ChatCompletionRequestMessage.new(
+        role: role,
+        content: content,
+        name: name,
+        function_call: function_call
+      )
+    end
+    LibTiktoken.c_num_tokens_from_messages(model, num_messages, messages_ptr).tap do |n|
+      raise "Error in num_tokens_from_messages" if n == LibC::SizeT::MAX
+    end
+  end
+
+  def self.chat_completion_max_tokens(model, messages)
+  end
 end
