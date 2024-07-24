c58e81fc4ae363f49fc820db63af3c042117656a
kojix2 <2xijok@gmail.com>
2024-07-24 04:15:50 +0000

    Fix example

--- examples/tanuki_special.cr
+++ examples/tanuki_special.cr
@@ -3,11 +3,11 @@ require "../src/tiktoken.cr"
 text = "吾輩は猫である。名前はたぬき。<|endoftext|>"
 
 encoding = Tiktoken.encoding_for_model("gpt-4")
-tokens = encoding.encode(text, allowed_special: [] of String)
+tokens = encoding.encode(text, allowed_special: Set(String).new)
 p tokens
 p encoding.decode(tokens)
 
 encoding = Tiktoken.encoding_for_model("gpt-4")
-tokens = encoding.encode(text, allowed_special: ["<|endoftext|>"])
+tokens = encoding.encode(text, allowed_special: ["<|endoftext|>"].to_set)
 p tokens
 p encoding.decode(tokens)
